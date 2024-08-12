4d4b127e0a371d5847c999ec2ff37929580fa9c1
Amogh-2404 <ramogh2404@gmail.com>
2024-08-12 13:05:54 +0530

    Implemented user follow system

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, get_user_model, login
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Contact, Profile
 from django.contrib import messages
+
 
 # Create your views here.
 
@@ -98,17 +100,41 @@ def edit(request):
         }
     )
 
+
 # Follow system and user detail and list views
 User = get_user_model()
+
+
 @login_required
 def user_list(request):
     users = User.objects.filter(is_active=True)
     # TODO:- Add pagination
     return render(request,
-                   'account/user/list.html',
-                   {'section':'people', 'users':users})
+                  'account/user/list.html',
+                  {'section': 'people', 'users': users})
+
 
 @login_required
 def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
-    return render(request,'account/user/detail.html',{'section':'people','user':user})
+    return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
+
+
+# Follow user
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_form=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_form=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'User does not exist'})
+
+    return JsonResponse({'status': 'error'})
