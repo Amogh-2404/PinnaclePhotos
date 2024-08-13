75212cb22b6f4e01778b00987e211cccc2c182a4
Amogh-2404 <ramogh2404@gmail.com>
2024-08-13 23:10:40 +0530

    Linked user actions for like, create account, follow and post

--- account/views.py
+++ account/views.py
@@ -6,7 +6,7 @@ from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditFor
 from django.contrib.auth.decorators import login_required
 from .models import Contact, Profile
 from django.contrib import messages
-
+from actions.utils import create_action
 
 # Create your views here.
 
@@ -55,6 +55,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user,'has created an account')
             return render(
                 request,
                 'account/register_done.html',
@@ -131,6 +132,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_form=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_form=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})
--- images/views.py
+++ images/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
-
+from actions.utils import create_action
 
 # Create your views here.
 
@@ -24,6 +24,7 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, 'bookmarked image', new_image)
             messages.success(request, "Image added successfully")
             # redirect to the new created item detail view
             return redirect(new_image.get_absolute_url())
@@ -54,6 +55,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user,'likes',image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
