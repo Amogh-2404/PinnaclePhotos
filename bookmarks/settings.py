942fd866619332862dcada2b315aa862a4c45bcb
Amogh-2404 <ramogh2404@gmail.com>
2024-08-18 11:34:05 +0530

    bug fixes

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('DB_HOST'),
     }
 }
