f47de5f4a5d934c3a6ce881c245294b4b49a5203
Amogh-2404 <ramogh2404@gmail.com>
2025-08-26 08:44:11 +0530

    Fixed bookmarklet.js

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -3,7 +3,10 @@
     if (!window.bookmarklet) {
         bookmarklet_js = document.body.appendChild(document.createElement('script'));
         {% load static %}
-        bookmarklet_js.src = '//{{ request.get_host }}{% static "js/bookmarklet.js" %}?r='
+        var host = '{{ request.get_host }}';
+        var isLocal = host.indexOf('localhost') === 0 || host.indexOf('127.0.0.1') === 0;
+        var prefix = isLocal ? 'http://' : '//';
+        bookmarklet_js.src = prefix + host + '{% static "js/bookmarklet.js" %}?r='
             + Math.floor(Math.random() * 9999999999999999)
             + '&css={% static "css/bookmarklet.css" %}';
         window.bookmarklet = true;
